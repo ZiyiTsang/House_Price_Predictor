7acedd206445ca7b2795a540cf9e5bfe5e716f87
ZIYI ZENG <CME1909120@XMU.EDU.MY>
2022-12-21 19:22:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# House_Price_Predictor
+ House_Price_Predictor on boston dataset
---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from autogluon.tabular import TabularPredictor
+import pandas as pd
+
+app = Flask(__name__)
+predictor = TabularPredictor.load('ModelSave')
+data = pd.read_csv(
+    'listing_preProcessing.csv')
+data = data.drop(columns=['price', 'Unnamed: 0'])
+
+
+@app.route("/", methods=['GET'])
+def intro():
+    return render_template('input.html')
+
+
+@app.route("/result", methods=['POST'])
+def result():
+    price_ = predict(request.form)
+    # print(request.form)
+    # print(predictPrice([8, 3], ['latitude', 'bedrooms']))
+    return render_template('result.html', price=price_)
+
+
+def predict(form):
+    value = []
+    columns = []
+    model = ''
+    for item in form:
+        if item == 'model':
+            model = form[item]
+            continue
+        columns.append(item)
+        number = form[item]
+        try:
+            number = float(number)
+        except:
+            number = number
+        finally:
+            value.append(number)
+    return predictPrice(value, columns, model_=model)
+
+
+def predictPrice(value, columnsName, model_='auto'):
+    print(model_)
+
+    global data
+    data = data[0:0]
+    df = pd.DataFrame([value],
+                       columns=columnsName)
+    df.loc[0:1,'host_since']=1
+    new_data = pd.concat([df, data])
+    if model_ == 'auto':
+        price = predictor.predict(new_data)
+    else:
+        price = predictor.predict(new_data, model=model_)
+    return float(price[0])
